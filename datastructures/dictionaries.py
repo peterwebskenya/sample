0315ea70dfc098a6e427c920ba70e17bfe3c7653
eMobilis <alphoncepeter2002@gmail.com>
2023-06-30 09:28:29 +0300

    dictionaries

--- datastructures/dictionaries.py
+++ datastructures/dictionaries.py
@@ -3,5 +3,6 @@
 #you can modify data in dictionaries
 
 friends_ages = { "roy":29, "jayden":20, "marie":30}
-friends_ages.add = {"tom"35}
+friends_ages.update({"tom":35})
+friends_ages["bob"] = 23
 print(friends_ages)
