a32c00e217539dc5be513800e798234d082d30b7
eMobilis <alphoncepeter2002@gmail.com>
2023-06-30 09:13:43 +0300

    list

---
+++ datastructures/dictionaries.py
@@ -0,0 +1,7 @@
+#has key and value pairs
+#also defined by curly brackets
+#you can modify data in dictionaries
+
+friends_ages = { "roy":29, "jayden":20, "marie":30}
+friends_ages.add = {"tom"35}
+print(friends_ages)
