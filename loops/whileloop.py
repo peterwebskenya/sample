63576a4475624bf8b7a710c4fd8e38b674ff28e4
eMobilis <alphoncepeter2002@gmail.com>
2023-07-05 10:19:27 +0300

    fizz,buzz,fizzbuzz

--- loops/whileloop.py
+++ loops/whileloop.py
@@ -1,4 +1,11 @@
-x = 2
-while x < 8:
-    print(x)
-    x += 1
+i = int(input("enter number"))
+v = int(input("enter second number"))
+for x in range (i,v + 1):
+    if x % 3 ==0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    elif(x % 3 == 0) and (x % 5 == 0):
+        print("fizzbuzz")
+    else:
+        print(x)
